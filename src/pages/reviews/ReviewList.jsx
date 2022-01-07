47374d0d1b5e0d6ef99cb3d9da0dcea378bbcc59
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-07 14:06:26 +0900

    axios를 활용해 review list api 호출

--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -1,4 +1,28 @@
+import Axios from 'axios';
+import { useEffect } from 'react';
+
 function ReviewList() {
+  //[]를 빼먹으면 컴포넌트 호출시에 매번 호출됨.
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  const refetch = () => {
+    const url = 'http://127.0.0.1:8000/shop/api/reviews/';
+    // Promise 객체
+    Axios.get(url)
+      .then((response) => {
+        console.group('정상 응답');
+        console.log(response);
+        console.groupEnd();
+      })
+      .catch((error) => {
+        console.group('에러 응답');
+        console.log(error);
+        console.groupEnd();
+      });
+  };
+
   return (
     <div>
       <h2>Review List</h2>
