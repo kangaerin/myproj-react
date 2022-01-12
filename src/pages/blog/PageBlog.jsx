72e39f19d76731668c94b979c0a0126c75fb905b
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-12 11:46:51 +0900

    블로그 과제1

--- src/pages/blog/PageBlog.jsx
+++ src/pages/blog/PageBlog.jsx
@@ -6,7 +6,7 @@ function PageBlog() {
       <h2 className="text-red-400 border-b-2 border-red-300">Blog</h2>
       <BlogList />
       <h3>제목 : </h3>
-      <input type={Text} /> <button>저장</button>
+      <input type={Text} />
       <h3>내용</h3>
       <textarea style={{ backgroundcolor: 'gray' }} />
     </div>
