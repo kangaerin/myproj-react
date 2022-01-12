173e76c410105f159c90301351ef8ec799387a94
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-12 14:58:02 +0900

    useApiAxios를 정의하고, ArticleForm 컴포넌트를 분리

--- src/components/news/ArticleDetail.jsx
+++ src/components/news/ArticleDetail.jsx
@@ -1,18 +1,18 @@
-import useAxios from 'axios-hooks';
 import { Link } from 'react-router-dom';
+import { useApiAxios } from 'api/base';
 
 function ArticleDetail({ articleId }) {
-  const [{ data: article, loading, error }] = useAxios(
-    `http://127.0.0.1:8000/news/api/articles/${articleId}/`,
+  const [{ data: article, loading, error }] = useApiAxios(
+    `/news/api/articles/${articleId}/`,
   );
-  // TODO: 데이터 통신
+
   return (
     <div>
-      {loading && '로딩중...'}
+      {loading && '로딩 중 ...'}
       {error && '에러가 발생했습니다.'}
       {article && (
         <>
-          <h3>{article.title}</h3>
+          <h3 className="text-2xl my-5">{article.title}</h3>
           <div>
             {article.content.split(/[\r\n]+/).map((line, index) => (
               <p className="my-3" key={index}>
@@ -23,11 +23,13 @@ function ArticleDetail({ articleId }) {
         </>
       )}
       <hr className="my-3" />
-      <div className="flex gap-4 mt-3 mv-10">
+      <div className="flex gap-4 mt-3 mb-10">
         <Link to="/news/" className="hover:text-red-400">
           목록으로
         </Link>
-        <Link to={`/news/{$articleId}/edit/`}>수정하기</Link>
+        <Link to={`/news/${articleId}/edit/`} className="hover:text-red-400">
+          수정하기
+        </Link>
       </div>
     </div>
   );
