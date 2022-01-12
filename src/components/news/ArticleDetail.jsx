78928fda3d8df68d6499a0d13b0e3dca8de14d65
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-12 16:19:38 +0900

    기사 삭제 기능 구현

--- src/components/news/ArticleDetail.jsx
+++ src/components/news/ArticleDetail.jsx
@@ -1,11 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useApiAxios } from 'api/base';
 import { useEffect } from 'react/cjs/react.development';
+import LoadingIndicator from 'components/LoadingIndicator';
 
 function ArticleDetail({ articleId }) {
+  const navigate = useNavigate();
   const [{ data: article, loading, error }, refetch] = useApiAxios(
     `/news/api/articles/${articleId}/`,
   );
+
+  const [{ loading: deleteLoading, error: deleteError }, deleteArticle] =
+    useApiAxios(
+      {
+        url: `/news/api/articles/${articleId}/`,
+        method: 'DELETE',
+      },
+      { manual: true },
+    );
+
+  const handleDelete = () => {
+    if (window.confirm('Are you sure?')) {
+      //REST API에서는 DELETE 요청에 대한 응답이 없습니다.
+      deleteArticle().then(() => navigate('/news/'));
+    }
+  };
 
   useEffect(() => {
     refetch();
@@ -13,8 +31,12 @@ function ArticleDetail({ articleId }) {
 
   return (
     <div>
-      {loading && '로딩 중 ...'}
-      {error && '에러가 발생했습니다.'}
+      {loading && <LoadingIndicator />}
+      {deleteLoading && <LoadingIndicator>삭제중 ...</LoadingIndicator>}
+      {error &&
+        `로딩중 에러가 발생했습니다.(${error.response.status} ${error.response.statusText})`}
+      {deleteError &&
+        `삭제 요청 중 에러가 발생했습니다.(${deleteError.response.status} ${deleteError.response.statusText})`}
       {article && (
         <>
           <h3 className="text-2xl my-5">{article.title}</h3>
@@ -35,6 +57,13 @@ function ArticleDetail({ articleId }) {
         <Link to={`/news/${articleId}/edit/`} className="hover:text-red-400">
           수정하기
         </Link>
+        <button
+          disabled={deleteLoading}
+          onClick={handleDelete}
+          className="hover:text-red-400"
+        >
+          삭제하기
+        </button>
       </div>
     </div>
   );
