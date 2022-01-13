6d76477873d9aee8bd502fbcb750475037c21c51
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-13 10:39:44 +0900

    article list/detail 컴포넌트에 photo 노출

--- src/components/news/ArticleSummary.jsx
+++ src/components/news/ArticleSummary.jsx
@@ -3,7 +3,14 @@ import { Link } from 'react-router-dom';
 function ArticleSummeary({ article }) {
   return (
     <div>
-      요약 :<Link to={`/news/${article.id}/`}>{article.title}</Link>
+      {article.photo && (
+        <img
+          src={article.photo}
+          alt={article.title}
+          className="mr-1 rounded w-5 h-5 inline"
+        />
+      )}
+      <Link to={`/news/${article.id}/`}>{article.title}</Link>
     </div>
   );
 }
