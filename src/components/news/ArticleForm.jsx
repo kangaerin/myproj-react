84ea41a5dda4e68be33a53d09f9a1d109718d695
kangaerin <95070081+kangaerin@users.noreply.github.com>
2022-01-13 11:16:49 +0900

    hook 변경, 포토 에러메세지

--- src/components/news/ArticleForm.jsx
+++ src/components/news/ArticleForm.jsx
@@ -41,8 +41,20 @@ function ArticleForm({ articleId, handleDidSave }) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    const formData = new FormData();
+    // filedValues : 객체 (except 파일)
+    // 파일을 업로드 하려면, FormData 인스턴스를 써야합니다.
+    Object.entries(fieldValues).forEach(([name, value]) => {
+      if (Array.isArray(value)) {
+        const fileList = value;
+        fileList.forEach((file) => formData.append(name, file));
+      } else {
+        formData.append(name, value);
+      }
+    });
+
     saveRequest({
-      data: fieldValues,
+      data: formData,
     }).then((response) => {
       const savedPost = response.data;
       if (handleDidSave) handleDidSave(savedPost);
@@ -64,7 +76,8 @@ function ArticleForm({ articleId, handleDidSave }) {
             value={fieldValues.title}
             onChange={handleFieldChange}
             type="text"
-            className="p-1 bg-gray-100 w-full outline-none focus:border focus:border-gray-400 focus:border-dashed"
+            className="p-1 bg-gray-100 w-full outline-none focus:border 
+            focus:border-gray-400 focus:border-dashed"
           />
           {saveErrorMessages.title?.map((message, index) => (
             <p key={index} className="text-xs text-red-400">
@@ -88,6 +101,18 @@ function ArticleForm({ articleId, handleDidSave }) {
         </div>
 
         <div className="my-3">
+          <input
+            type="file"
+            accept=".png, .jpg, .jpeg"
+            name="photo"
+            // value="" < 의미x
+            onChange={handleFieldChange}
+          />
+          {saveErrorMessages.photo?.map((message, index) => (
+            <p key={index} className="text-xs text-red-400">
+              {message}
+            </p>
+          ))}
           <Button>저장하기</Button>
         </div>
       </form>
