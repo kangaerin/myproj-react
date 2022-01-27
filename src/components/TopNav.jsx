895956eb277a7aa70ed0579b553cd4f7195e0449
kangaerin <rosyty2@naver.com>
2022-01-27 11:34:35 +0900

    에러 해결

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -4,8 +4,6 @@ import { useAuth } from 'contexts/AuthContext';
 function TopNav() {
   const [auth, , , logout] = useAuth();
 
-  console.log(auth);
-
   const handledLogout = () => {
     logout();
   };
@@ -34,14 +32,6 @@ function TopNav() {
         <MyLink to="/blog/">블로그</MyLink>
         <MyLink to="/news/">뉴스룸</MyLink>
         <MyLink to="/telephone_book/">주소록</MyLink>
-
-        {/* <MyLink to="/reviews/">리뷰</MyLink>
-        <MyLink to="/examples/components/">컴포넌트 예시</MyLink>
-        <MyLink to="/examples/clock/">시계</MyLink>
-        <MyLink to="/examples/Css-module/">Css</MyLink>
-        <MyLink to="/examples/cssinjs/">CssInJs</MyLink>
-        <MyLink to="/examples/context-api/">Context API</MyLink>
-        <MyLink to="/examples/context-api-2/">Context API #2</MyLink> */}
       </ul>
     </div>
   );
